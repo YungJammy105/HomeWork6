411c1a2d2c55e9ee7502214c3893fce17085abd3
Ilya Andreev <Franklylakers@yandex.ru>
2022-12-15 23:10:23 +0700

    add Example41

---
+++ Example41/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Enter array elements separated by a space: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"The number of positive elements in the array: {count}");
